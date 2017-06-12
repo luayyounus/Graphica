09d4983d4e610a7b2a9cf8e7e82ce03a20dde6bc
Luay Younus <luayghazwan@yahoo.com>
2017-06-11 22:13:06 -0700

    Circle model drawing, set axis, draw, canvas

---
+++ src/main/java/com/google/Circle.java
@@ -0,0 +1,25 @@
+package com.google;
+import java.awt.*;
+
+/**
+ * Created by luayghazwan on 6/11/17.
+ */
+public class Circle extends Shape {
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw(Graphics canvas) {
+        canvas.setColor(color);
+
+        if (isFilled) {
+            canvas.fillOval(x-radius+1, y-radius+1, radius*2, radius*2);
+        } else {
+            canvas.drawOval(x-radius+1, y-radius+1, radius*2, radius*2);
+        }
+    }
+}
